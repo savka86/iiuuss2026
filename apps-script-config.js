2d62519dc125bc4f38452be639685758caaa0006
savka86 <64674641+savka86@users.noreply.github.com>
2026-06-20 08:03:25 +0900

    Update apps-script-config.js

--- apps-script-config.js
+++ apps-script-config.js
@@ -3,7 +3,7 @@
 // Пример:
 // window.APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycb.../exec";
 
-window.APPS_SCRIPT_WEB_APP_URL = "";
+window.APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycby9Anpm9R2hSBTDD5spluaaP-9gyiaiz2Gnfik2nPnSK-M2xhV2rFYNbo2Mt0k2nrMFzA/exec";
 
 // Токен оставлен только для совместимости со старыми версиями.
 // В новом Code.gs он не блокирует отправку.
